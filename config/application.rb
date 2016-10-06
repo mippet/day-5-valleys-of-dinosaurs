5d2c8c3d54a97a21b1b6d53753e72ed4cf3e4e4e
p.luitjes <pimluitjes@gmail.com>
2016-10-06 09:41:26 +0200

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module WorldOfDinosaurs
+  class Application < Rails::Application
+    # ...
+
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
